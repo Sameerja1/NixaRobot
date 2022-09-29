81270eaf8233374bf70ea92ef8b96bb8f1f5e4b5
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <107318123+Sumit9969@users.noreply.github.com>
2022-09-30 01:12:44 +0530

    Add files via upload

--- Nixa/__main__.py
+++ Nixa/__main__.py
@@ -1,36 +1,31 @@
-import os 
+import os
 import logging
 import pyrogram
-from os import gentenv
-from pyrogram Client
-import config
+from os import environ, getenv
+from pyrogram import Client
+import config 
 from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
 
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.WARNING,
-)
-
-
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID", "0"))
+API_ID = getenv("API_ID")
+API_HASH = getenv("API_HASH")
+OWNER_ID = int(getenv("OWNER_ID", 0))
 
 
-
-linda = "🎉 sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇᴘʟᴏʏ ʏᴏᴜʀ ʙᴏᴛ !!"
-tinda = "😂 ᴀʀᴇ ʙʜᴀɪ ᴀʙ ʙᴏᴛ ʙɴ ɢʏᴀ ʜᴀɪ ᴄʜᴀɴɴᴇʟ ᴊᴏɪɴ ᴋʀʟᴇ"
 
 if __name__ == "__main__" :
-   print(linda)
-   print(tinda)
-   app = pyrogram.Client(
-        "Nixa",
+    print("Starting Bot...")
+    
+    app = pyrogram.Client(
+        "Nixa",        
         config.API_ID,
         config.API_HASH,
         bot_token=BOT_TOKEN,
-        plugins=dict(root="plugins")
-)
-     
-        app.run()
+        plugins = dict(root="Nixa/plugins")
+ 
+        )
+                          
+app.run()          
+ 
