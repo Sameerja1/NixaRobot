6e1a58154d6e044f00b9f77e6fe1ba580ef1e4dd
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <107318123+Sumit9969@users.noreply.github.com>
2022-09-30 01:05:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "9369978"))
+API_HASH = getenv("API_HASH", "c49c6216f8b01b1d9dc501fee2f34bee")
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", ""))
