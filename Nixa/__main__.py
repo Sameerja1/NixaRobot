0627a47e6048737e909c55a0b9566e0465f6f7ee
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <107318123+Sumit9969@users.noreply.github.com>
2022-09-30 00:45:19 +0530

    Update __main__.py

--- Nixa/__main__.py
+++ Nixa/__main__.py
@@ -1,1 +1,36 @@
-#rukjaa bhai kr rha hu
+import os 
+import logging
+import pyrogram
+from os import gentenv
+from pyrogram Client
+import config
+from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.WARNING,
+)
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+OWNER_ID = int(getenv("OWNER_ID", "0"))
+
+
+
+linda = "🎉 sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇᴘʟᴏʏ ʏᴏᴜʀ ʙᴏᴛ !!"
+tinda = "😂 ᴀʀᴇ ʙʜᴀɪ ᴀʙ ʙᴏᴛ ʙɴ ɢʏᴀ ʜᴀɪ ᴄʜᴀɴɴᴇʟ ᴊᴏɪɴ ᴋʀʟᴇ"
+
+if __name__ == __"main"__:
+   print(linda)
+   print(tinda)
+   app = pyrogram.Client(
+        "Nixa",
+        config.API_ID,
+        config.API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins=dict(root="plugins")
+)
+     
+        app.run()
