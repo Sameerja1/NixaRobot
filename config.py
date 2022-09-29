367edd875f5c555168fc39c11e871137437d7d19
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <107318123+Sumit9969@users.noreply.github.com>
2022-09-30 01:09:40 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-import os
+import os 
 from os import getenv
 
-
-API_ID = int(getenv("API_ID", "9369978"))
-API_HASH = getenv("API_HASH", "c49c6216f8b01b1d9dc501fee2f34bee")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", "7507532"))
+API_HASH = getenv("API_HASH", "c4f37d9aea37ef639f1f2da178acec3d")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", ""))
